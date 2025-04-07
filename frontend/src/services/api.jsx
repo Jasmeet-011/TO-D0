bfe73862778dbae0ad66d7faa8e241e49f5b6ce4
Jasmeet-011 <jasmeetsingh5003@gmail.com>
2025-04-06 21:09:21 -0400

    Updated the port setting in api.jsx

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,11 +1,10 @@
-// src/services/api.jsx
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-export default api;
+export default API;
